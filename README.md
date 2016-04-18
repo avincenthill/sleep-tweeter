e59f599eeda1ea0252f566d91167302a87892bed
avincenthill <alexanderjvincenthill@gmail.com>
2016-04-17 21:15:03 -0700

    First commit to Github for Heroku deploy

---
+++ README.md
@@ -0,0 +1,2 @@
+# sleep-tweeter
+This app takes data from my fitbit and tweets when I fall asleep/wake up
---
+++ index.js
@@ -0,0 +1,44 @@
+// START HEROKU SETUP
+var express = require("express");
+var app = express();
+app.get('/', function(req, res){ res.send('The robot is happily running.'); });
+app.listen(process.env.PORT || 5000);
+// END HEROKU SETUP
+
+
+// Listbot config
+//
+// Config.keys uses environment variables so sensitive info is not in the repo.
+var config = {
+    me: 'sleep_tweeter', // The authorized account with a list to retweet.
+    myList: 'favpornstars', // The list we want to retweet.
+    regexFilter: '', // Accept only tweets matching this regex pattern.
+    regexReject: '(RT|@)', // AND reject any tweets matching this regex pattern.
+
+    keys: {
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    },
+};
+
+// What to do after we tweet something.
+function onTweet(err) {
+    if(err) {
+        console.error("tweeting failed :(");
+        console.error(err);
+    }
+}
+
+// The application itself.
+// Use the tuiter node module to get access to twitter.
+var tu = require('tuiter')(config.keys);
+
+// Run the application.
+setInterval(function() {
+    tu.update({
+            status: "It is " + Date()
+    }, onTweet);
+    console.log('Tweeting at ' + Date());
+}, 30000);
